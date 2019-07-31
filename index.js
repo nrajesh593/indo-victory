0ec6077c741956d1e9bd87244d331ed31723bcab
nrajesh593 <nrajesh593@gmail.com>
2019-07-31 13:06:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const Discord=require('discord.js');
-const bot = new Discord.Client();
-const token='NjA1Njg2MjYxOTMxNDQyMTk2.XUE7rA.tiPsKTImPMXAwwplaBCzpWdGHdk';
-const Prefix='!';
+const Discord = require('discord.js');
 
-bot.on('ready', () =>{
-    console.log("this bot is ready");
-})
-bot.on('message',message=>{
-    let args = message.content.substring(Prefix.length).split(" ");
-    switch(args[0]){
-        case 'embed':
-         const embed =new Discord.RichEmbed()
-         .setTitle('User Information')
-        .addField('Player Name',message.author.username)
-        .addField('Version ','1.0.1')
-        .addField('Current Server', message.guild.name)
-        .setColor(0xFFFF00)
-        .setThumbnail(message.author.avatarURL)
-        .setTimestamp()
-        message.channel.sendEmbed(embed);
-        break;
+const client = new Discord.Client();
 
-    }
-    
-})
+ 
 
-bot.login(token);
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
